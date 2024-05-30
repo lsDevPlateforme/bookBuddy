e91b5b59687005c949473dab0f7b9451135ff8b9
Liron Saiman <163527712+lsDevPlateforme@users.noreply.github.com>
2024-05-30 11:12:21 +0200

    Delete client directory

--- client/src/components/book/HeaderBook.jsx
+++
@@ -1,9 +0,0 @@
-import { ModalAddBook } from "./ModalAddBook";
-
-export const HeaderBook = () => {
-  return (
-    <div className="flex justify-end">
-      <ModalAddBook />
-    </div>
-  );
-};
--- client/src/components/ui/loader.jsx
+++
@@ -1,6 +0,0 @@
-import { cn } from "@/lib/utils";
-import { Loader2 } from "lucide-react";
-
-export const Loader = ({ className, ...props }) => {
-  return <Loader2 className={cn("animate-spin", className)} {...props} />;
-};
