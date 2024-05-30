2f5c6be4fdd013d204059b456cca78008ec0e14c
Adam-Cherkani <adam.cherkani@laplateforme.io>
2024-05-30 10:36:33 +0200

    git pMerge branch 'main' of https://github.com/lsDevPlateforme/bookBuddy

--- client/src/pages/user/Favorite.jsx
+++ client/src/pages/user/Favorite.jsx
@@ -3,7 +3,7 @@ import { Separator } from "@/components/ui/separator";
 export const FavoritePage = () => {
   return (
     <>
-      <h2 className="text-lg mt-4">Tous mes Favorie:</h2>
+      <h2 className="text-lg mt-4">Tous mes Favoris:</h2>
       <Separator />
     </>
   );
